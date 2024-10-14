fb85d8b80d64fc74f4ec7a6047411e7c00c461ae
Christian Grothe <christian.grothe@posteo.de>
2024-10-14 21:48:45 +0200

    cross

---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd server
+
+echo "compiling for aarch64"
+cross build --target aarch64-unknown-linux-gnu --release
+
+echo "copying binary to pi"
+scp target/aarch64-unknown-linux-gnu/release/server \
+christian@satellites.local:/home/christian/satellites/server_arm64
+
+echo "building frontend"
+
+cd ../frontend
+
+npm run build
+
+echo "copying frontend to pi"
+scp -r dist/ christian@satellites.local:/home/christian/satellites/
+
+ssh christian@satellites.local 'cd /home/christian/satellites/ && ./deploy.sh && sudo systemctl restart satellites.service'
